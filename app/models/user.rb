21a7cceda6b7baf82abafb4c4b05f081057eb6d9
John van Arkelen <johnvanarkelen@gmail.com>
2013-03-20 07:55:55 +0100

    locale added + simple_form working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   include Clearance::User
+
+  attr_accessible :first_name, :last_name, :nickname, :email, :role, :language
+
+  validates :first_name, :last_name, :nickname, :email, :role, :language, presence: true
 end
