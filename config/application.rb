105bef931d6bddbcbd43593635083d71c71a98bd
John van Arkelen <johnvanarkelen@gmail.com>
2013-04-04 08:01:02 +0200

    prepared for heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -62,7 +62,6 @@ module Frenzy
     # Load lib
     config.autoload_paths += %W(#{config.root}/lib)
 
-    config.assets.initialize_on_precompile = false
-
+    config.assets.initialize_on_precompile=false
   end
 end
