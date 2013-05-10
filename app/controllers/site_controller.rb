6ebe52ec957cfd1f434016a47abf27083bdacbfb
John van Arkelen <johnvanarkelen@gmail.com>
2013-05-10 15:36:23 +0200

    working on graphs

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,5 +3,6 @@ class SiteController < ApplicationController
     @sticky_newsitems = Newsitem.sticky
     @newsitems = Newsitem.top3.paginate(page: params[:page])
     @general_rankings = Ranking.calculate_ranking("general")
+    @clubs = Club.all
   end
 end
