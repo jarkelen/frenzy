45143bbe6e31039bd0e11bdb2e29ff5c1bc1a04c
John van Arkelen <johnvanarkelen@gmail.com>
2013-05-29 09:51:25 +0200

    added joker canellation to spec

--- spec/features/calculations_spec.rb
+++ spec/features/calculations_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe "Frenzy calculations" do
-  before :each do
+  before do
     init_settings
     @admin = create_user('admin')
     sign_in_as(@admin)
   end
 
-  context "switching participation" do
+  describe "switching participation" do
     it "should turn off participation" do
       visit frenzy_index_path
       page.should have_selector("input[type=submit][value='Deelname uit']")
@@ -28,8 +28,8 @@ describe "Frenzy calculations" do
     end
   end
 
-  context "participation on" do
-    before :each do
+  describe "participation on" do
+    before do
       visit frenzy_index_path
       click_button("Deelname uit")
     end
@@ -40,29 +40,51 @@ describe "Frenzy calculations" do
       page.should_not have_selector("input[type=submit][value='Verwijderen']")
     end
   end
+
+  describe "joker cancellation" do
+    let!(:gameround1) { create :gameround, number: 1, processed: false }
+    let!(:gameround2) { create :gameround, number: 2, processed: true }
+    let!(:club1)     { create :club, club_name: "Arsenal" }
+    let!(:club2)     { create :club, club_name: "Chelsea" }
+    let!(:club3)     { create :club, club_name: "Fulham" }
+    let!(:joker1)    { create :joker, club: club1, gameround: gameround1, user: @admin }
+    let!(:joker2)    { create :joker, club: club2, gameround: gameround1, user: @admin }
+    let!(:joker3)    { create :joker, club: club3, gameround: gameround1, user: @admin }
+    let!(:score1)    { create :score, club: club1, gameround: gameround1 }
+    let!(:score2)    { create :score, club: club2, gameround: gameround1 }
+    let!(:score3)    { create :score, club: club3, gameround: gameround1 }
+
+    it "should show all jokers for active gameround" do
+      visit jokers_path
+      page.should have_content(club1.club_name)
+      page.should have_content(club2.club_name)
+    end
+
+    it "should show used joker count" do
+      visit jokers_path
+      page.should have_content("Jokers verbruikt: 3 van de #{@admin.assigned_jokers}")
+    end
+
+    it "should not show cancelled jokers" do
+      visit jokers_path
+      page.should have_content("Jokers verbruikt: 3 van de #{@admin.assigned_jokers}")
+
+      visit frenzy_index_path
+      select club1.club_name, from: 'line_1_home_club_id'
+      select club2.club_name, from: 'line_1_away_club_id'
+      click_button 'Jokers annuleren'
+
+      visit jokers_path
+      page.should_not have_content(club1.club_name)
+      page.should_not have_content(club2.club_name)
+      page.should have_content(club3.club_name)
+      page.should have_content("Jokers verbruikt: 1 van de #{@admin.assigned_jokers}")
+    end
+
+  end
 end
 
 =begin
-
-  let!(:setting) { create :setting }
-
-  before :each do
-    @calculator = Calculator.new
-  end
-
-  describe "new" do
-    it "returns a Calculator object" do
-      @calculator.should be_an_instance_of Calculator
-    end
-  end
-
-  describe "process_gameround" do
-    let!(:gameround) { create :gameround }
-    let!(:club1)     { create :club }
-    let!(:club2)     { create :club }
-    let!(:club3)     { create :club }
-    let!(:club4)     { create :club }
-    let!(:result1)   { create :result, home_club_id: club1.id, away_club_id: club2.id, home_score: 2, away_score: 1, gameround_id: gameround.id }
-    let!(:result2)   { create :result, home_club_id: club3.id, away_club_id: club4.id, home_score: 0, away_score: 3, gameround_id: gameround.id }
-
+      let!(:result1)   { create :result, home_club_id: club1.id, away_club_id: club2.id, home_score: 2, away_score: 1, gameround_id: gameround.id }
+      let!(:result2)   { create :result, home_club_id: club3.id, away_club_id: club4.id, home_score: 0, away_score: 3, gameround_id: gameround.id }
 =end
