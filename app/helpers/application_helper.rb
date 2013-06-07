78ac90dbe26b5991face38c800c522cd113e8f30
John van Arkelen <johnvanarkelen@gmail.com>
2013-06-07 19:49:14 +0200

    improved avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,16 +9,16 @@ module ApplicationHelper
   end
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user, size)
+  def gravatar_for(user, size = :small)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 
     case size
-      when "small"
+      when :small
         size = 50
       else
         size = 200
     end
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm&r=dg"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm&r=pg"
     image_tag(gravatar_url, alt: user.full_name)
   end
 
