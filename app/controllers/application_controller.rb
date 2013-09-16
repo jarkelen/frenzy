ee621c63d79f09b191fcc7162362ef1ad08297c2
John van Arkelen <johnvanarkelen@gmail.com>
2013-09-16 13:13:39 +0200

    added www redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Clearance::Controller
   protect_from_forgery
   before_filter :set_locale
+  before_filter :add_www_subdomain
 
   WillPaginate.per_page = 25
 
@@ -9,11 +10,19 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, notice: I18n.t('.general.not_authorized')
   end
 
+  private
+
   def set_locale
     if signed_in?
       I18n.locale = current_user.language
     else
       I18n.locale = I18n.default_locale
+    end
+  end
+
+  def add_www_subdomain
+    unless /^www/.match(request.host)
+      redirect_to("#{request.protocol}x.com#{request.request_uri}", status: 301)
     end
   end
 
