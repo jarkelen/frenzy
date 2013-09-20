da3687c27c7b150a60c96b64dc2cc7b3f74d4938
John van Arkelen <johnvanarkelen@gmail.com>
2013-09-20 17:42:46 +0200

    removed www checker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   include Clearance::Controller
   protect_from_forgery
   before_filter :set_locale
-  before_filter :add_www_subdomain
 
   WillPaginate.per_page = 25
 
@@ -17,12 +16,6 @@ class ApplicationController < ActionController::Base
       I18n.locale = current_user.language
     else
       I18n.locale = I18n.default_locale
-    end
-  end
-
-  def add_www_subdomain
-    unless /^www/.match(request.host)
-      redirect_to("#{request.protocol}x.com#{request.request_uri}", status: 301)
     end
   end
 
